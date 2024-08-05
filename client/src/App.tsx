4b9e785c63c157b2bf871b88a3934ddfb685e31d
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-08-05 16:52:21 +0530

    css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,36 @@
-
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
-import './App.css'
-import Dashboard from "./Pages/dashboard"
-import Auth from "./Pages/auth"
-import { FinincialRecordsProvider } from "./context/financial-record-context"
-import { SignedIn, UserButton,} from "@clerk/clerk-react"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Dashboard from "./Pages/dashboard";
+import Auth from "./Pages/auth";
+import { FinincialRecordsProvider } from "./context/financial-record-context";
+import { SignedIn, UserButton } from "@clerk/clerk-react";
 
 function App() {
-  return <Router>
-    <div className="app-container">
-      <div className="navbar">
-        <h2>FinT</h2>
-        {/* <Link to="/">Dashboard</Link> */}
-      <SignedIn>
-        <UserButton showName/>
-      </SignedIn>
-      </div>
-      <Routes>
-        <Route path="/dashboard" element={<FinincialRecordsProvider>
-          <Dashboard/>
-          </FinincialRecordsProvider>
-        } 
-        />
-        <Route path="/" element={<Auth />} />
-      </Routes>
-    </div>
-  </Router>
-
   return (
-    <>
-      hi
-        
-    </>
-  )
+    <Router>
+      <div className="app-container">
+        <SignedIn>
+          <div className="navbar">
+            <h2>FinT</h2>
+            {/* <Link to="/">Dashboard</Link> */}
+
+            <UserButton showName />
+          </div>
+        </SignedIn>
+        <Routes>
+          <Route
+            path="/dashboard"
+            element={
+              <FinincialRecordsProvider>
+                <Dashboard />
+              </FinincialRecordsProvider>
+            }
+          />
+          <Route path="/" element={<Auth />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- client/src/Pages/auth/index.tsx
+++ client/src/Pages/auth/index.tsx
@@ -1,11 +1,28 @@
-import { SignedIn, SignedOut, SignInButton, SignUpButton,} from "@clerk/clerk-react";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+} from "@clerk/clerk-react";
 import { Navigate } from "react-router-dom";
-
 
 function Auth() {
   return (
     <>
-    <div className="sign-in-container">
+      <div className="navbar">
+        <h2>FinT</h2>
+        {/* <Link to="/">Dashboard</Link> */}
+        <div className="sign-in-container">
+          <SignedOut>
+            <SignUpButton mode="modal" />
+            <SignInButton mode="modal" />
+          </SignedOut>
+          <SignedIn>
+            <Navigate to={"/dashboard"} />
+          </SignedIn>
+        </div>
+      </div>
+      {/* <div className="sign-in-container">
       <SignedOut>
         <SignUpButton mode="modal"/>
         <SignInButton  mode="modal"/>
@@ -14,13 +31,16 @@ function Auth() {
         <Navigate to={"/dashboard"}/>
       </SignedIn>
   
-    </div>
-    <h1>Finance Management App</h1>
-    <div className="hero">
-    <img src="https://readme-typing-svg.demolab.com?font=Poppins&size=42&weight=500&height=57&pause=700&width=800&lines=Here+you+can+Manage+Dashboard;Here+you+can+Manage+Expences;Here+you+can+Manage+Savings" alt="Typing SVG" />
-    </div>
+    </div> */}
+      <h1>Finance Management App</h1>
+      <div className="hero">
+        <img
+          src="https://readme-typing-svg.demolab.com?font=Poppins&size=42&weight=500&height=57&pause=700&width=800&lines=Here+you+can+Manage+Dashboard;Here+you+can+Manage+Expences;Here+you+can+Manage+Savings"
+          alt="Typing SVG"
+        />
+      </div>
     </>
-  )
+  );
 }
 
-export default Auth
+export default Auth;
